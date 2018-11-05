49fae41d5c6993217e6e49b12a0b9903c8958531
Ari-Matti Nivasalo <ari-matti.nivasalo@wunderdog.fi>
2018-11-05 13:20:21 +0200

    Add categories indexeddb entry, save default category by transceiver

---
+++ src/common/categories/categoriesReducer.js
@@ -0,0 +1,24 @@
+
+import {
+  ADD_CATEGORY,
+  RECEIVE_CATEGORIES,
+} from '../constants'
+
+const defaultState = {
+  defaultTransceiverCategories: [],
+}
+
+export default (state = defaultState, action) => {
+  const { payload } = action
+
+  switch (action.type) {
+    case (RECEIVE_CATEGORIES):
+      return { defaultTransceiverCategories: payload }
+    case (ADD_CATEGORY):
+      const newState = {...state}
+      newState.defaultTransceiverCategories.push(payload)
+      return newState
+  }
+
+  return state
+}
